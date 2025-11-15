c67613344b3bbd5304a56f8612585380eb8750d0
Madison Lawson <mlawson4@terpmail.umd.edu>
2025-11-15 15:48:05 -0500

    images and tasklist

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,8 @@
-// Hardcoded list of tasks
-const tasks = [
-  "Wake up early",
-  "Make breakfast",
-  "Work on Zenith website",
-  "Take a break",
-  "Finish homework"
-];
+const tasks = ["one", "two", "three", "five"];
+const taskList = document.getElementById("tasklist");
 
-// Grab the UL
-const list = document.getElementById("tasklist");
-
-// Insert tasks into the UL
 tasks.forEach(task => {
   const li = document.createElement("li");
   li.textContent = task;
-  list.appendChild(li);
+  taskList.appendChild(li);
 });
